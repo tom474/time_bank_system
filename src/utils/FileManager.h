1aa12ea43d9f19f831d790e5a39e365202725761
tom474 <emsignouc0407@gmail.com>
2023-12-20 09:36:19 +0700

    TBA-7 create header file

---
+++ src/utils/FileManager.h
@@ -0,0 +1,31 @@
+#ifndef _TIME_MANAGER_H_
+#define _TIME_MANAGER_H_
+
+#include "../models/user/User.h"
+#include "../models/user/Admin.h"
+#include "../models/user/Member.h"
+#include "../models/service/Availability.h"
+#include "../models/service/Request.h"
+#include "../models/service/Review.h"
+#include "../models/service/Skill.h"
+#include "./TimeManager.h"
+
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::vector;
+
+void saveMemberDatabase(vector<Member*>);
+void saveAvailabilityDatabase(vector<Availability*>);
+void saveRequestDatabase(vector<Request*>);
+void saveReviewDatabase(vector<Review*>);
+void saveSkillDatabase(vector<Skill*>);
+Admin loadAdminDatabase();
+vector<Member> loadMemberDatabase();
+vector<Availability> loadAvailabilityDatabase();
+vector<Request> loadRequestDatabase();
+vector<Review> loadReviewDatabase();
+vector<Skill> loadSkillDatabase();
+
+#endif
