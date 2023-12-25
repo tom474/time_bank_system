8dadfb41f8376844719d28dcd36d5451bfb3d7ed
locoioioi <118866346+locoioioi@users.noreply.github.com>
2023-12-25 14:14:07 +0700

    Update include paths in Menu.h

--- src/views/Menu.h
+++ src/views/Menu.h
@@ -1,10 +1,10 @@
 #ifndef _MENU_H_
 #define _MENU_H_
 
-#include "GuestController.h"
-#include "MenuOptionsGenerator.h"
-#include "MemberController.h"
-#include "AdminController.h"
+#include "../controllers/GuestController.h"
+#include "../utils/MenuOptionsGenerator.h"
+#include "../controllers/MemberController.h"
+#include "../controllers/AdminController.h"
 #include "../models/user/Member.h"
 #include "../utils/FileManager.h"
 
